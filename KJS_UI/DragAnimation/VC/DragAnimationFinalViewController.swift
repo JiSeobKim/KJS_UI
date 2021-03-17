488b0506e392b71431de2f6ba4b49c44f2ac5304
김지섭 <jiseob812@gmail.com>
2021-03-17 19:55:08 +0900

    DragAnimation Final 진행중

---
+++ KJS_UI/DragAnimation/VC/DragAnimationFinalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DragAnimationFinalViewController.swift
+//  KJS_UI
+//
+//  Created by 김지섭 on 2021/03/17.
+//  Copyright © 2021 JS. All rights reserved.
+//
+
+import UIKit
+
+class DragAnimationFinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
